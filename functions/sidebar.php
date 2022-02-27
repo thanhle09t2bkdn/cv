94e73168bc194bb00ce538f5f0ef96c769633b66
thanhle <thanhle09t2bkdn@gmail.com>
2022-02-27 21:19:38 +0700

    init

---
+++ functions/sidebar.php
@@ -0,0 +1,50 @@
+<?php
+// SIDEBARS AND WIDGETIZED AREAS
+function joints_register_sidebars() {
+	register_sidebar(array(
+		'id' => 'sidebar1',
+		'name' => __('Sidebar 1', 'cv'),
+		'description' => __('The first (primary) sidebar.', 'cv'),
+		'before_widget' => '<div id="%1$s" class="widget %2$s">',
+		'after_widget' => '</div>',
+		'before_title' => '<h4 class="widgettitle">',
+		'after_title' => '</h4>',
+	));
+
+	register_sidebar(array(
+		'id' => 'offcanvas',
+		'name' => __('Offcanvas', 'cv'),
+		'description' => __('The offcanvas sidebar.', 'cv'),
+		'before_widget' => '<div id="%1$s" class="widget %2$s">',
+		'after_widget' => '</div>',
+		'before_title' => '<h4 class="widgettitle">',
+		'after_title' => '</h4>',
+	));
+
+	/*
+	to add more sidebars or widgetized areas, just copy
+	and edit the above sidebar code. In order to call
+	your new sidebar just use the following code:
+
+	Just change the name to whatever your new
+	sidebar's id is, for example:
+
+	register_sidebar(array(
+		'id' => 'sidebar2',
+		'name' => __('Sidebar 2', 'cv'),
+		'description' => __('The second (secondary) sidebar.', 'cv'),
+		'before_widget' => '<div id="%1$s" class="widget %2$s">',
+		'after_widget' => '</div>',
+		'before_title' => '<h4 class="widgettitle">',
+		'after_title' => '</h4>',
+	));
+
+	To call the sidebar in your template, you can just copy
+	the sidebar.php file and rename it to your sidebar's name.
+	So using the above example, it would be:
+	sidebar-sidebar2.php
+
+	*/
+} /* end register sidebars */
+
+add_action( 'widgets_init', 'joints_register_sidebars' );
---
+++ parts/content-missing.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * The template part for displaying a message that posts cannot be found
+ */
+?>
+
+<div class="post-not-found">
+	
+	<?php if ( is_search() ) : ?>
+		
+		<header class="article-header">
+			<h1><?php _e( 'Sorry, No Results.', 'cv' );?></h1>
+		</header>
+		
+		<section class="entry-content">
+			<p><?php _e( 'Try your search again.', 'cv' );?></p>
+		</section>
+		
+		<section class="search">
+		    <p><?php get_search_form(); ?></p>
+		</section> <!-- end search section -->
+		
+		<footer class="article-footer">
+			<p><?php _e( 'This is the error message in the parts/content-missing.php template.', 'cv' ); ?></p>
+		</footer>
+		
+	<?php else: ?>
+	
+		<header class="article-header">
+			<h1><?php _e( 'Oops, Post Not Found!', 'cv' ); ?></h1>
+		</header>
+		
+		<section class="entry-content">
+			<p><?php _e( 'Uh Oh. Something is missing. Try double checking things.', 'cv' ); ?></p>
+		</section>
+		
+		<section class="search">
+		    <p><?php get_search_form(); ?></p>
+		</section> <!-- end search section -->
+		
+		<footer class="article-footer">
+		  <p><?php _e( 'This is the error message in the parts/content-missing.php template.', 'cv' ); ?></p>
+		</footer>
+			
+	<?php endif; ?>
+	
+</div>
